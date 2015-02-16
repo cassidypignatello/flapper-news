300f277666d40b6bc4a36d0521bbc0cb62076b03
cassidypignatello <cassidy.pignatello@gmail.com>
2015-02-16 18:27:04 -0500

    allows submission of links to title

--- js/app.js
+++ js/app.js
@@ -1,20 +1,19 @@
 angular.module('flapperNews', [])
 .controller('MainCtrl', ['$scope', function($scope) {
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+  $scope.posts = [];
 
   $scope.addPost = function() {
     if (!$scope.title || $scope.title === '') {
       return alert("Your post must have a title!");
     };
 
-    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0
+    });
     $scope.title = '';
+    $scope.link = '';
   };
 
   $scope.incrementUpvotes = function(post) {
